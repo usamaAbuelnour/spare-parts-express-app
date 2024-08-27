77d53f88b233187d1b9b547bf838277232519a8c
Usama-Abuelnour <usama.abuelnour@gmail.com>
2024-08-27 18:49:02 +0300

    adding name field to the login/register response

--- controllers/user.js
+++ controllers/user.js
@@ -15,16 +15,16 @@ const register = async (req, res) => {
         throw new CustomError(422, error.message);
     }
 
-    const { firstName, lastName, email, password } = req.body;
+    const { firstName, email, password } = req.body;
     const existingUser = await UserModel.findOne({ email });
 
     if (existingUser) throw new CustomError(409, "Email already exists!!");
 
-    const name = `${firstName} ${lastName}`;
-    const user = await UserModel.create({ name, email, password });
+    const user = await UserModel.create({ name: firstName, email, password });
     if (user) {
         res.status(201).send({
             id: user._id,
+            name: user.name,
             email: user.email,
             token: await generateToken(user._id),
         });
@@ -41,15 +41,13 @@ const login = async (req, res) => {
     const { email, password } = req.body;
     const user = await UserModel.findOne({ email });
 
-    
-
     if (user && (await bcrypt.compare(password, user.password))) {
-
-        if (user && user.role === "admin")
+        if (user.role === "admin")
             throw new CustomError(403, "Admins are not welcome here!!");
 
         res.send({
             id: user._id,
+            name: user.name,
             email: user.email,
             token: await generateToken(user._id),
         });
